44c63d787d2485c3dd7522ee1315ec3a5c4c3d08
Alexey Seleznev <selesnow@mail.ru>
2021-11-22 11:57:08 +0200

    1. Добавил функцию ao_get_leaners_with_score()

---
+++ R/ao_get_leaners_With_score.R
@@ -0,0 +1,36 @@
+#' This returns a list of learners filtered by their score
+#'
+#' @param score Filter score the ratio of completed content to available content, across all courses/groups
+#' @param operator One of the following comparison operators '=', '>', '<', '>=', '<=', '<>'
+#'
+#' @return tibble with leaners
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' leaners <- ao_get_leaners_with_score(
+#'     score = 20,
+#'     operator = '>'
+#' )
+#' }
+ao_get_leaners_with_score  <- function(
+  score = 1,
+  operator = c('>=', '>', '<', '=', '<=', '<>')
+) {
+
+  cli_alert_info('Compose request body')
+  rbody <- oa_make_body(
+    action = 'learnersWithScore',
+    options = c('score', 'operator'),
+    values = c(score,  match.arg(operator))
+  )
+
+  cli_alert_info('Send request')
+  resp <- oa_request(body = rbody, token = suppressMessages(ao_auth()))
+  cli_alert_info('Parse result')
+  res <- oa_parser(resp)
+  cli_alert_success('Loaded {nrow(res)} learners')
+
+  return(res)
+
+}
